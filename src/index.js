5520ccee99f6114e7140f5924cbaba2fb4ced523
TrainerGamer <62667610+TrainerGamer@users.noreply.github.com>
2021-03-19 19:19:45 +0000

    Automatically create ./files if directory does not exist

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const app = express();
 const helmet = require("helmet");
 const cors = require("cors");
 const fileUpload = require("express-fileupload");
+var fs = require('fs');
 require('dotenv').config()
 
 const images = require("./routes/images");
@@ -16,5 +17,10 @@ app.use(cors());
 app.use(express.json());
 app.use("/", images);
 app.use('/static', express.static(__dirname + '/static'))
+
+var dir = '../files';
+if (!fs.existsSync(dir)){
+    fs.mkdirSync(dir);
+}
 
 module.exports = app.listen(process.env.APP_PORT, () => console.log(`The API is now running on port ${process.env.APP_PORT}`));
